49587647a9d1bcd44bedbfe1f18e6d5d68fc7f8c
Brian Moriarty <morbrian@me.com>
2015-12-09 19:35:21 -0800

    added public/private key export, works on my home mac, but had to enter password.

--- pki/pki-testing.sh
+++ pki/pki-testing.sh
@@ -21,6 +21,13 @@ runExportPublic() {
     keytool -export -alias $ALIAS -file ${ALIAS}.cer -storepass $PASSWORD -keystore $KEYSTORE_FILE
 }
 
+runExportPrivate() {
+    ALIAS=$1
+    PASSWORD=$2
+    KEYSTORE_FILE=$3
+
+    keytool -importkeystore -srckeystore $KEYSTORE_FILE -destkeystore ${ALIAS}.p12 -srcstoretype JKS -deststoretype PKCS12 -srcstorepass $PASSWORD -deststorepass $PASSWORD -srcalias $ALIAS -destalias $ALIAS -srckeypass $PASSWORD -destkeypass $PASSWORD -noprompt
+}
 runTrustPublic() {
     ALIAS=$1
     PASSWORD=$2
@@ -36,6 +43,8 @@ createNewIdentity() {
 
     $(runExportPublic $ALIAS $PASSWORD $USER_KEYSTORE)
 
+    $(runExportPrivate $ALIAS $PASSWORD $USER_KEYSTORE)
+
     $(runTrustPublic $ALIAS $PASSWORD)
 
 }
@@ -50,12 +59,12 @@ createServerKeystore() {
 # Create a bunch of sample users
 $(createNewIdentity moriarty Brian_Moriarty)
 $(createNewIdentity fring "Gus_Fring")
-$(createNewIdentity grimes "Rick_Grimes")
-$(createNewIdentity white "Walter_White")
-$(createNewIdentity trump "Donald_Trump")
-$(createNewIdentity sanders "Bernie_Sanders")
-$(createNewIdentity clinton "Hillary_Clinton")
-$(createNewIdentity carson "Ben_Carson")
+#$(createNewIdentity grimes "Rick_Grimes")
+#$(createNewIdentity white "Walter_White")
+#$(createNewIdentity trump "Donald_Trump")
+#$(createNewIdentity sanders "Bernie_Sanders")
+#$(createNewIdentity clinton "Hillary_Clinton")
+#$(createNewIdentity carson "Ben_Carson")
 
 # Create the server cert
 $(createServerKeystore mpnov.sd.spawar.navy.mil mpnov.sd.spawar.navy.mil)
