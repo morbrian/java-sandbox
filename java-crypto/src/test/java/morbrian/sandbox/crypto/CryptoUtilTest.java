53db0a8080f759478ae7301e66288e4425b8bebd
Brian Moriarty <morbrian@me.com>
2015-12-16 21:51:30 -0800

    added crypto module, removed superfluous build blocks from all poms.

---
+++ java-crypto/src/test/java/morbrian/sandbox/crypto/CryptoUtilTest.java
@@ -0,0 +1,22 @@
+package morbrian.sandbox.crypto;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class CryptoUtilTest {
+
+  @Test public void testEncryptDecryptAes() throws Exception {
+
+    String sampleText = "Hello World!";
+
+    int aesKeyLength = 128;
+    String aesKey = CryptoUtil.generateAesKeyString(aesKeyLength);
+    String encryptedText = CryptoUtil.encryptAes(sampleText, aesKey, aesKeyLength);
+    String decryptedText = CryptoUtil.decryptAes(encryptedText, aesKey, aesKeyLength);
+
+    assertEquals(sampleText, decryptedText);
+  }
+
+}
